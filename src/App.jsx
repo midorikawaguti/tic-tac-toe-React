9dd2ee192e46964d8f484ca5ef8271076433e1b1
Tatiana Kawaguti <tuemurak@my.centennialcollege.ca>
2024-07-12 09:13:56 -0400

    Added Player Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import Player from './components/Player.jsx';
 
 function App() {
   
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+      <Player playerName="Player 1" symbol="X"/>
+      <Player playerName="Player 2" symbol="O"/>
+      
+      </ol>
+      GAME BOARD
+    </div>
+
+    LOG
+    </main>
+    
 }
 
 export default App
