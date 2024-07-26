26298fde2679c232c10d0451d81e0bc56c15cf29
Tatiana Kawaguti <tuemurak@my.centennialcollege.ca>
2024-07-26 11:36:20 -0400

    The "Game Over" Screen & Checking for a Draw

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import { useState } from 'react';
 import Player from './components/Player.jsx';
 import GameBoard from './components/GameBoard.jsx';
 import Log from './components/Log.jsx';
+import { WINNING_COMBINATIONS } from './winning-combinations.js';
+import GameOver from './components/GameOver.jsx';
+
+const initialGameBoard = [
+    [null , null, null],
+    [null, null, null],
+    [null, null, null]
+]
 
 function deriveActivePlayer(gameTurns){
   let currentPlayer = 'X';
@@ -15,8 +23,32 @@ function deriveActivePlayer(gameTurns){
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for(const turn of gameTurns){
+      const { square, player } = turn;
+      const { row, col } = square;
+
+      gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS){
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol  = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol  = gameBoard[combination[2].row][combination[2].column];
+
+    if (firstSquareSymbol  && 
+      firstSquareSymbol === secondSquareSymbol && 
+      firstSquareSymbol === thirdSquareSymbol){
+        winner = firstSquareSymbol;
+      }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex){
 
@@ -38,10 +70,9 @@ function App() {
       <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
       
       </ol>
+      {(winner || hasDraw) && <GameOver winner={winner}/>}
       <GameBoard
-        onSelectSquare={handleSelectSquare} 
-        
-        turns={gameTurns}/>
+        onSelectSquare={handleSelectSquare} board={gameBoard}/>
     </div>
 
     <Log turns={gameTurns}/>
