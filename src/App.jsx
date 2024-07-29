c04196510acdb2fb529e7bbc78ee42f30ac38164
Tatiana Kawaguti <tuemurak@my.centennialcollege.ca>
2024-07-29 09:52:24 -0400

    Added fuctionality to restart the game

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for(const turn of gameTurns){
       const { square, player } = turn;
@@ -63,6 +63,10 @@ function App() {
     });
   }
 
+  function handleRestart(){
+    setGameTurns([]);
+  }
+
   return <main>
     <div id="game-container">
       <ol id="players" className='highlight-player'>
@@ -70,7 +74,7 @@ function App() {
       <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
       
       </ol>
-      {(winner || hasDraw) && <GameOver winner={winner}/>}
+      {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
       <GameBoard
         onSelectSquare={handleSelectSquare} board={gameBoard}/>
     </div>
